1f3961dee758899f0c852be91015fcb43f4f340b
Rosie Gonzalez <rgonzalez@xpanxion.com>
2023-06-22 15:55:12 -0800

    example 1

--- exercises/ex1.py
+++ exercises/ex1.py
@@ -1,2 +1,7 @@
-def ex1():
-    print("who's there")
+
+def hello_world():
+    string = int(input("Enter your string here"))
+    for i in range(string):
+        print("hello world from Python!")
+
+hello_world()
